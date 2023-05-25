e3226240b69368fc62554394e669ec0f7facbd6a
Plashchun <yurii.plashchun.ukr@net>
2023-05-25 15:30:08 +0300

    Add file

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ else:
 #3
 text = input("Введіть рядок: ")
 if text.startswith("abc"):
-  text = text.replace("abc", "www")#
+  text = text.replace("abc", "www")
 else:
   text += "qqq"
 print(text)
